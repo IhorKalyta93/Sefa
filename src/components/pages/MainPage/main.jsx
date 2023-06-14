8dced507e2474fb6c528fb3032b5f4c164e8bd59
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-06-14 20:25:38 +0200

    1.0.3

---
+++ src/components/pages/MainPage/main.jsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <div className={s.HomePage}>
+      <head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>MainPage</h1>
+</body>
+    </div>
+  );
+}
