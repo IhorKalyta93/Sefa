481ecd991196869f43215f24c490f42a130abf6e
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-06-26 21:01:42 +0200

    1010

--- src/components/pages/MainPage/portfolio.jsx
+++ src/components/pages/MainPage/portfolio.jsx
@@ -1,29 +1,5 @@
 export default function HomePage() {
   return (
-<head>
-    <title>Product List</title>
-    <style>
-        /* CSS styles for the product list */
-        .product {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin-bottom: 10px;
-        }
-        .product img {
-            max-width: 100px;
-            max-height: 100px;
-        }
-        .product h3 {
-            margin: 0;
-        }
-        .product p {
-            margin: 5px 0;
-        }
-    </style>
-</head>
-<body>
-   
-    
-</body>
+
   );
 }
